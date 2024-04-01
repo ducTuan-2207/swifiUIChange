3136cc3f94d34bcad3bc3e100974460e6a76362c
DucTuan <tuanhypc02@gmail.com>
2024-04-01 23:59:26 +0700

    Initial Commit

---
+++ SwiftUIChanges/SwiftUIChangesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUIChangesApp.swift
+//  SwiftUIChanges
+//
+//  Created by macOS on 01/04/2024.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUIChangesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
